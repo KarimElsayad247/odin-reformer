8e2514bbbffdfd83cb1fddd74f9c5c733b33dc55
KarimElsayad247 <karimelsayad1999@gmail.com>
2022-10-04 15:08:16 +0200

    Put form params in a hash
    Modify the controller to allow this.
    
    Commented out line will be used later.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,21 @@
 class UsersController < ApplicationController
   def create
-    @user = User.new(
-      username: params[:username],
-      email: params[:email],
-      password: params[:password]
-    )
+    # @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
       redirect_to new_user_path
     else
+      pp @user.errors.full_messages
       render :new, status: :unprocessable_entity
     end
   end
 
   def new
 
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
